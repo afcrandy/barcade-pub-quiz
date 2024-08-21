b5495a61fdf566f3c18856fb6f8ec775ca19ef59
Andrew Reid <andrew.o.reid@gmail.com>
2024-08-21 14:34:13 +0000

    Add code to retrieve questions from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
+// create the data store for retrieved questions
+let questionData = {};
+
 // Wait for the DOM to finish loading
 document.addEventListener("DOMContentLoaded", function() {
+    // add instructions modal event listeners
     let instructionsOpenButton = document.getElementById('instructions-btn');
     let instructionsCloseButton = document.getElementById('close-instructions-btn');
 
@@ -18,3 +22,17 @@ function closeInstructions() {
     instructions.close();
 }
 
+// call question api and retrieve 15 random questions
+async function retrieveQuestions() {
+    // build api call, default to 15 questions no category
+    const apiBaseURL = 'https://the-trivia-api.com/v2/questions?limit=15';
+    const apiResponse = await fetch(apiBaseURL);
+
+    // handle call failure
+    if (apiResponse.status === 200) {
+        questionData = await apiResponse.json();
+    } else {
+        // add error page here
+        console.log('API call failed');
+    }
+}
