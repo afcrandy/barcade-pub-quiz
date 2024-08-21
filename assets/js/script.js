6b22a7422ee31d02513e54f50a013c76e81c1f0d
Andrew Reid <andrew.o.reid@gmail.com>
2024-08-21 17:30:10 +0000

    Code quiz start and writing first question to screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // create the data store for retrieved questions
-let questionData = {};
+let questionsData = {};
 
 // Wait for the DOM to finish loading
 document.addEventListener("DOMContentLoaded", function() {
@@ -9,6 +9,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     instructionsOpenButton.addEventListener('click', openInstructions);
     instructionsCloseButton.addEventListener('click', closeInstructions);
+
+    // add beginQuiz event listener to the 'Play' buttons
+    let quickPlayButton = document.getElementById('quick-play-btn');
+    quickPlayButton.addEventListener('click', beginQuiz);
 })
 
 // event listeners to open and close the instructions modal
@@ -33,9 +37,44 @@ async function retrieveQuestions(categories) {
 
     // handle call failure
     if (apiResponse.status === 200) {
-        questionData = await apiResponse.json();
+        questionsData = await apiResponse.json();
+        writeQuestion(0);
     } else {
         // add error page here
         console.log('API call failed');
     }
 }
+
+// begin the quiz - retrieve the questions and load up the first question
+// future: add code to reshape the page
+function beginQuiz(categories) {
+    retrieveQuestions(categories);
+
+    // rearrange the screen for the quiz
+
+    // set the scores to 0
+    let scoreTracker = document.getElementById('score-tracker');
+    scoreTracker.innerHTML = '0';
+}
+
+// given an index write a question to the page
+function writeQuestion(questionIndex) {
+    let questionData = questionsData[questionIndex];
+
+    // write question text to qText
+    let qText = document.getElementById('question-text');
+    qText.innerHTML = questionData.question.text;
+
+    // collect correct and incorrect answers together and sort alphabetically
+    let answersList = [questionData.correctAnswer, ...questionData.incorrectAnswers];
+    answersList.sort();
+    
+    // write the answers list to the option areas in alphabetical order
+    for (let [index, answer] of answersList.entries()) {
+        // retrieve the option element and update
+        let option = document.getElementById(`answer-${index}`);
+        option.innerHTML = answer;
+    }
+}
+
+// 
